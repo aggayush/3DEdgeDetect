aadec71f179e922c1c38031dacd547434b69ca1e
Ayush <agg.ayush234@gmail.com>
2019-11-28 12:23:29 +0000

    Fixed minor changes Added code for reading file and converting it to voxel

--- Source/utilities.py
+++ Source/utilities.py
@@ -5,32 +5,35 @@
 import argparse
 from os import listdir
 from os.path import isfile, join
+import open3d
 
 
 def arg_creator():
     parser = argparse.ArgumentParser(description='Arguments for the edge detector')
-    parser.add_argument(name='--train-data-path',
+    parser.add_argument('--train-data-path',
                         default='./Data/Train/',
                         dest='trainDataPath',
                         help='Path to the training data files')
-    parser.add_argument(name='--test-data-path',
+    parser.add_argument('--test-data-path',
                         default='./Data/Test/',
                         dest='testDataPath',
                         help='Path to the testing data files')
-    parser.add_argument(name='--output-path',
+    parser.add_argument('--output-path',
                         default='./Output/',
                         dest='outputPath',
                         help='Path to the output folder')
-    parser.add_argument(name='--model-path',
+    parser.add_argument('--model-path',
                         default='./Model/',
                         dest='modelPath',
                         help='Path to the model save folder (same for loading pretrained model)')
-    parser.add_argument(name='--is-train',
+    parser.add_argument('--is-train',
                         action='store_true',
+                        default=True,
                         dest='isTrain',
                         help='To run training or testing')
-    parser.add_argument(name='--is-streamed',
+    parser.add_argument('--is-streamed',
                         action='store_true',
+                        default=False,
                         dest='isStreamed',
                         help='if data is streamed input or to be read from file')
 
@@ -42,6 +45,12 @@ def path_reader(path):
     if path is None:
         return None
 
-    fileList = [f for f in listdir(path) if isfile(join(path,f))]
+    fileList = [join(path, f) for f in listdir(path) if isfile(join(path, f))]
 
     return fileList
+
+def visualize(points):
+
+    pcd = open3d.geometry.PointCloud()
+    pcd.points = open3d.utility.Vector3dVector(points)
+    open3d.visualization.draw_geometries([pcd])
--- ThreeDEdgeDetect.py
+++ ThreeDEdgeDetect.py
@@ -2,13 +2,16 @@
 # Main class file to execute the code
 ################################
 
-
 import open3d
 import tensorflow as tf
-from Source.utilities import path_reader
+import numpy as np
+from Source.utilities import path_reader, visualize
 
 
 class ThreeDEdgeDetector:
+    VOXEL_GRID_X = 256
+    VOXEL_GRID_Y = 256
+    VOXEL_GRID_Z = 256
 
     def __init__(self,args=None):
         if args is None:
@@ -26,6 +29,39 @@ class ThreeDEdgeDetector:
             self.isTrain = args.isTrain
             self.isStreamed = args.isStreamed
 
+        self.dataSize = 0
+
+    # function to create the voxel grid and return
+    def preprocessing(self, data):
+        grid = np.zeros((self.VOXEL_GRID_X, self.VOXEL_GRID_Y, self.VOXEL_GRID_Z), dtype=np.bool)
+
+        #Normalizing in unit sphere
+        centroid = np.mean(data, axis=0)
+        data = data - centroid
+        furthestDistanceFromCentre = np.max(np.sqrt(np.sum(abs(data)**2, axis=-1)))
+        data = data/furthestDistanceFromCentre
+
+        #visualize(data)
+
+        #Voxelization of data
+        gridX = np.floor(data[:, 0]*(self.VOXEL_GRID_X/2) + self.VOXEL_GRID_X/2)
+        gridY = np.floor(data[:, 1]*(self.VOXEL_GRID_X/2) + self.VOXEL_GRID_X/2)
+        gridZ = np.floor(data[:, 2]*(self.VOXEL_GRID_X/2) + self.VOXEL_GRID_X/2)
+
+        indexes = (tuple(gridX.astype(int)), tuple(gridY.astype(int)), tuple(gridZ.astype(int)))
+
+        grid[indexes] = True
+
+        # visualization
+        # gridX = (gridX - self.VOXEL_GRID_X/2)/(self.VOXEL_GRID_X/2)
+        # gridY = (gridY - self.VOXEL_GRID_Y/2)/(self.VOXEL_GRID_Y/2)
+        # gridZ = (gridZ - self.VOXEL_GRID_X/2)/(self.VOXEL_GRID_Z/2)
+        # points = np.stack((gridX, gridY, gridZ), axis=1)
+        # points = np.unique(points, axis=0)
+        # visualize(points)
+
+        return grid
+
     def prepare_data(self):
 
         if self.isTrain:
@@ -37,10 +73,12 @@ class ThreeDEdgeDetector:
 
         fileList = path_reader(path)
 
-        print(fileList)
+        self.dataSize = len(fileList)
+        data = open3d.io.read_point_cloud(fileList[0])
+        data = np.asarray(data.points)
+        data = self.preprocessing(data)
 
+    def run(self):
 
-     def run(self):
+        self.prepare_data()
 
-         self.prepare_data()
-        
