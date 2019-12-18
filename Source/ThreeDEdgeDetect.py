bbba6f323c9e592a16ecf98be165baf8a5d1b0b3
Ayush <agg.ayush234@gmail.com>
2019-12-18 18:44:01 +0000

    Network running without error. Testing and verifying now

--- Source/ThreeDEdgeDetect.py
+++ Source/ThreeDEdgeDetect.py
@@ -8,21 +8,24 @@ import os
 import tensorflow.keras as tfk
 import math
 import glob
+import numpy as np
 from Source.CustamLayer import SobelFilter, MergePointCloud
 from tensorflow.python.framework.ops import disable_eager_execution
-from Source.utilities import path_reader, visualize
+from Source.utilities import path_reader, visualize_from_file
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
 
 # disable_eager_execution()
 
 
 class ThreeDEdgeDetector:
-    VOXEL_GRID_X = 32
-    VOXEL_GRID_Y = 32
-    VOXEL_GRID_Z = 32
+    VOXEL_GRID_X = 128
+    VOXEL_GRID_Y = 128
+    VOXEL_GRID_Z = 128
     DATA_DEFAULTS = [[0.], [0.], [0.]]
 
-    def __init__(self,args=None):
+    def __init__(self, args=None):
         if args is None:
             self.trainDataPath = './Data/Train/'
             self.testDataPath = './Data/Test/'
@@ -57,7 +60,7 @@ class ThreeDEdgeDetector:
             self.learningRate = args.learningRate
             self.epochs = args.epochs
             self.trainValRatio = args.trainValRatio
-        self.prefetchBufferSize=10
+        self.prefetchBufferSize = 10
         self.trainDataset = None
         self.testDataset = None
         self.model = None
@@ -70,25 +73,27 @@ class ThreeDEdgeDetector:
         rawData = tf.io.read_file(filePath)
         records = tf.strings.split(rawData, sep='\r\n')
         data = tf.io.decode_csv(records,
-                               self.DATA_DEFAULTS,
-                               field_delim=' ')
+                                self.DATA_DEFAULTS,
+                                field_delim=' ')
         # Normalization to unit sphere
         data = tf.transpose(data)
         centroid = tf.math.reduce_mean(data, axis=0)
         data = data - centroid
-        furthestDistanceFromCentre = tf.math.reduce_max(tf.math.sqrt(tf.math.reduce_sum(tf.math.square(tf.math.abs(data)), axis=-1)))
+        furthestDistanceFromCentre = tf.math.reduce_max(
+            tf.math.sqrt(tf.math.reduce_sum(tf.math.square(tf.math.abs(data)), axis=-1)))
         data = tf.math.divide(data, furthestDistanceFromCentre)
 
-        #Voxelization of data
-        gridX = tf.floor(tf.add(tf.multiply(data[:, 0], (self.VOXEL_GRID_X/2)), self.VOXEL_GRID_X/2))
-        gridY = tf.floor(tf.add(tf.multiply(data[:, 1], (self.VOXEL_GRID_Y/2)), self.VOXEL_GRID_Y/2))
-        gridZ = tf.floor(tf.add(tf.multiply(data[:, 2], (self.VOXEL_GRID_Z/2)), self.VOXEL_GRID_Z/2))
+        # Voxelization of data
+        gridX = tf.floor(tf.add(tf.multiply(data[:, 0], (self.VOXEL_GRID_X / 2)), self.VOXEL_GRID_X / 2))
+        gridY = tf.floor(tf.add(tf.multiply(data[:, 1], (self.VOXEL_GRID_Y / 2)), self.VOXEL_GRID_Y / 2))
+        gridZ = tf.floor(tf.add(tf.multiply(data[:, 2], (self.VOXEL_GRID_Z / 2)), self.VOXEL_GRID_Z / 2))
 
         indexes = tf.stack([gridX, gridY, gridZ], axis=1)
         indexes = tf.cast(indexes, tf.int64)
         shape = tf.shape(indexes)
         sparseValues = tf.ones([shape[0]], tf.float32)
-        delta = tf.sparse.SparseTensor(indices=indexes, values=sparseValues, dense_shape=[self.VOXEL_GRID_X, self.VOXEL_GRID_Y, self.VOXEL_GRID_Z])
+        delta = tf.sparse.SparseTensor(indices=indexes, values=sparseValues,
+                                       dense_shape=[self.VOXEL_GRID_X, self.VOXEL_GRID_Y, self.VOXEL_GRID_Z])
         grid = tf.math.add(grid, tf.sparse.to_dense(delta, default_value=False, validate_indices=False))
         grid = tf.expand_dims(grid, -1)
 
@@ -113,25 +118,27 @@ class ThreeDEdgeDetector:
 
         self.trainDataset = trainFilesDs.map(lambda filePath: self.process_data_files(filePath))
         self.trainDataset = self.trainDataset.shuffle(self.shuffleBufferSize)
-        self.valDataset = self.trainDataset.take(math.ceil(self.trainValRatio*numElements))
+        self.valDataset = self.trainDataset.take(math.ceil(self.trainValRatio * numElements))
 
-        self.trainDataset = self.trainDataset.skip(math.ceil(self.trainValRatio*numElements))
+        self.trainDataset = self.trainDataset.skip(math.ceil(self.trainValRatio * numElements))
         self.trainDataset = self.trainDataset.batch(self.batchSize)
         self.trainDataset = self.trainDataset.prefetch(self.prefetchBufferSize)
 
+        self.valDataset = self.valDataset.batch(1)
+        self.valDataset = self.valDataset.prefetch(self.prefetchBufferSize)
 
         self.testDataset = testFilesDs.map(lambda filePath: self.process_data_files(filePath))
         self.testDataset = self.testDataset.shuffle(self.shuffleBufferSize)
-        self.testDataset = self.testDataset.batch(self.batchSize)
+        self.testDataset = self.testDataset.batch(1)
         self.testDataset = self.testDataset.prefetch(self.prefetchBufferSize)
 
         # # to see filename and visualize data
         # for f in trainFilesDs.take(1):
         #     print(f.numpy())
-        #     visualize(f.numpy().decode("utf-8"))
+        #     visualize_from_file(f.numpy().decode("utf-8"))
         # for f in testFilesDs.take(1):
         #     print(f.numpy())
-        #     visualize(f.numpy().decode("utf-8"))
+        #     visualize_from_file(f.numpy().decode("utf-8"))
 
     def load_weights(self):
 
@@ -143,92 +150,140 @@ class ThreeDEdgeDetector:
 
     def create_model(self):
 
-        input = tfk.Input(shape=(self.VOXEL_GRID_X, self.VOXEL_GRID_Y, self.VOXEL_GRID_Z, 1), batch_size=self.batchSize)
+        input = tfk.Input(shape=(self.VOXEL_GRID_X, self.VOXEL_GRID_Y, self.VOXEL_GRID_Z, 1))
 
         layers1 = tfk.layers.Conv3D(64,
-                                   (3, 3, 3),
-                                   padding='same',
-                                   activation=self.activation,
-                                   input_shape=(self.VOXEL_GRID_X, self.VOXEL_GRID_Y, self.VOXEL_GRID_Z, 1),
-                                   kernel_initializer=tf.initializers.glorot_normal)(input)
-        layers1 = tfk.layers.BatchNormalization()(layers1)
+                                    (3, 3, 3),
+                                    padding='same',
+                                    strides=(1, 1, 1),
+                                    activation=self.activation,
+                                    input_shape=(self.VOXEL_GRID_X, self.VOXEL_GRID_Y, self.VOXEL_GRID_Z, 1),
+                                    kernel_initializer=tf.initializers.glorot_normal,
+                                    name='Conv1')(input)
+        # layers1 = tfk.layers.BatchNormalization()(layers1)
         layers1 = tfk.layers.Dropout(self.dropoutRate)(layers1)
         layers1 = tfk.layers.Conv3D(1,
-                                   (3, 3, 3),
-                                   strides=(1, 1, 1),
-                                   padding='same',
-                                   activation=self.activation,
-                                   input_shape=(self.VOXEL_GRID_X, self.VOXEL_GRID_Y, self.VOXEL_GRID_Z, 1),
-                                   kernel_initializer=tf.initializers.glorot_normal)(layers1)
-        layers1 = tfk.layers.BatchNormalization()(layers1)
+                                    (3, 3, 3),
+                                    strides=(2, 2, 2),
+                                    padding='same',
+                                    activation=self.activation,
+                                    name='Conv2')(layers1)
+        # layers1 = tfk.layers.BatchNormalization()(layers1)
         layers1 = tfk.layers.Dropout(self.dropoutRate)(layers1)
         edge1 = SobelFilter(name='edge_1', trainable=False)(layers1)
-        out1 = tfk.layers.Activation(activation='softmax')(edge1)
+        layers1 = tfk.layers.Activation(activation=self.activation)(edge1)
 
         layers2 = tfk.layers.Conv3D(64,
                                     (3, 3, 3),
-                                    strides=(2, 2, 2),
+                                    strides=(1, 1, 1),
                                     padding='same',
                                     activation=self.activation,
-                                    input_shape=(self.VOXEL_GRID_X, self.VOXEL_GRID_Y, self.VOXEL_GRID_Z, 1),
-                                    kernel_initializer=tf.initializers.glorot_normal)(edge1)
-        layers2 = tfk.layers.BatchNormalization()(layers2)
+                                    kernel_initializer=tf.initializers.glorot_normal,
+                                    name='Conv3')(layers1)
+        # layers2 = tfk.layers.BatchNormalization()(layers2)
         layers2 = tfk.layers.Dropout(self.dropoutRate)(layers2)
         layers2 = tfk.layers.Conv3D(1,
                                     (3, 3, 3),
+                                    strides=(2, 2, 2),
+                                    padding='same',
+                                    activation=self.activation,
+                                    kernel_initializer=tf.initializers.glorot_normal,
+                                    name='Conv4')(layers2)
+        # layers2 = tfk.layers.BatchNormalization()(layers2)
+        layers2 = tfk.layers.Dropout(self.dropoutRate)(layers2)
+        edge2 = SobelFilter(name='edge_2', trainable=False)(layers2)
+        layers2 = tfk.layers.Activation(activation=self.activation)(edge2)
+
+        layers3 = tfk.layers.Conv3D(64,
+                                    (3, 3, 3),
                                     strides=(1, 1, 1),
                                     padding='same',
                                     activation=self.activation,
-                                    input_shape=(self.VOXEL_GRID_X, self.VOXEL_GRID_Y, self.VOXEL_GRID_Z, 1),
-                                    kernel_initializer=tf.initializers.glorot_normal)(layers2)
-        layers2 = tfk.layers.BatchNormalization()(layers2)
-        layers2 = tfk.layers.Dropout(self.dropoutRate)(layers2)
-        edge2 = SobelFilter(name='edge_2', trainable=False)(layers2)
-        out2 = tfk.layers.Activation(activation='softmax')(edge2)
-
-        layers3 = tfk.layers.Conv3D(64,
+                                    kernel_initializer=tf.initializers.glorot_normal,
+                                    name='Conv5')(layers2)
+        # layers3 = tfk.layers.BatchNormalization()(layers3)
+        layers3 = tfk.layers.Dropout(self.dropoutRate)(layers3)
+        layers3 = tfk.layers.Conv3D(1,
                                     (3, 3, 3),
                                     strides=(2, 2, 2),
                                     padding='same',
                                     activation=self.activation,
-                                    input_shape=(self.VOXEL_GRID_X, self.VOXEL_GRID_Y, self.VOXEL_GRID_Z, 1),
-                                    kernel_initializer=tf.initializers.glorot_normal)(edge2)
-        layers3 = tfk.layers.BatchNormalization()(layers3)
+                                    kernel_initializer=tf.initializers.glorot_normal,
+                                    name='Conv6')(layers3)
+        # layers3 = tfk.layers.BatchNormalization()(layers3)
         layers3 = tfk.layers.Dropout(self.dropoutRate)(layers3)
-        layers3 = tfk.layers.Conv3D(1,
+        edge3 = SobelFilter(name='edge_3', trainable=False)(layers3)
+        layers4 = MergePointCloud([self.VOXEL_GRID_X, self.VOXEL_GRID_Y, self.VOXEL_GRID_Z],
+                                  'avg',
+                                  name='output_1',
+                                  trainable=False)([edge1, edge2, edge3])
+        # layers4 = tfk.layers.Conv3D(2,
                                     (3, 3, 3),
                                     strides=(1, 1, 1),
                                     padding='same',
-                                    activation=self.activation,
-                                    input_shape=(self.VOXEL_GRID_X, self.VOXEL_GRID_Y, self.VOXEL_GRID_Z, 1),
-                                    kernel_initializer=tf.initializers.glorot_normal)(layers3)
-        layers3 = tfk.layers.BatchNormalization()(layers3)
-        layers3 = tfk.layers.Dropout(self.dropoutRate)(layers3)
-        edge3 = SobelFilter(name='edge_3', trainable=False)(layers3)
-        out3 = tfk.layers.Activation(activation='softmax')(edge3)
-        outFinal = MergePointCloud([self.VOXEL_GRID_X, self.VOXEL_GRID_Y, self.VOXEL_GRID_Z],
-                                   'avg',
-                                   name='output_1',
-                                   trainable=False)([out1, out2, out3])
+                                    kernel_initializer=tf.initializers.glorot_normal,
+                                    name='Conv7')(layers4)
+        outFinal = tfk.layers.Activation(activation='relu')(layers4)
 
         self.model = tfk.Model(inputs=input, outputs=outFinal)
-
-        opt = tfk.optimizers.Adam(learning_rate=self.learningRate)
-        loss = tfk.losses.BinaryCrossentropy()
-        self.model.compile(optimizer=opt,
-                           loss=loss,
-                           metrics=['accuracy', tfk.metrics.MeanIoU(num_classes=2)])
 
         self.model.summary()
 
     def train(self):
 
-        tensorboardCallback = tfk.callbacks.TensorBoard(log_dir=self.logDir, histogram_freq=1)
+        opt = tfk.optimizers.Adam(learning_rate=self.learningRate)
+        loss = tfk.losses.MeanSquaredError()
 
-        self.model.fit(self.trainDataset,
-                       epochs=self.epochs,
-                       validation_data=(self.valDataset, self.valDataset),
-                       callbacks=[tensorboardCallback])
+        def calc_loss(inp, tar):
+            tar_ = self.model(inp)
+
+            return loss(y_true=tar, y_pred=tar_)
+
+        def grad(inp, tar):
+            with tf.GradientTape() as tape:
+                loss_value = calc_loss(inp, tar)
+            return tape.gradient(loss_value, self.model.trainable_variables)
+
+        for epoch in range(self.epochs):
+            epochTrainLossAvg = tfk.metrics.Mean()
+            epochTrainAccuracy = tfk.metrics.BinaryAccuracy()
+
+            epochValLossAvg = tfk.metrics.Mean()
+            epochValAccuracy = tfk.metrics.BinaryAccuracy()
+
+            for x, c, f in self.trainDataset:
+                pred = self.model(x)
+                lossVal = calc_loss(x, x)
+                epochTrainLossAvg.update_state(lossVal)
+                epochTrainAccuracy.update_state(x, pred)
+
+                grads = grad(x, x)
+                opt.apply_gradients(zip(grads, self.model.trainable_variables))
+
+            for x, c, f in self.valDataset:
+                lossVal = calc_loss(x, x)
+                pred = self.model(x)
+                epochValLossAvg.update_state(lossVal)
+                epochValAccuracy.update_state(x, pred)
+
+            print("Epoch {:03d}: Train_Loss: {:.3f}, Train_Accuracy: {:.3%}, "
+                  "Val_Loss: {:.3f}, Val_Accuracy: {:.3%},".format(epoch,
+                                                                   epochTrainLossAvg.result(),
+                                                                   epochTrainAccuracy.result(),
+                                                                   epochValLossAvg.result(),
+                                                                   epochValAccuracy.result()))
+
+    def test(self):
+
+        for x, c, f in self.testDataset:
+            pred = self.model(x)
+            pred = pred.numpy()
+            pred = np.squeeze(pred, axis=-1)
+            pred = np.squeeze(pred, axis=0)
+            indexX, indexY, indexZ = np.where(pred > 0.2)
+            pointsX = indexX - self.VOXEL_GRID_X/2
+
+
 
     def run(self):
 
@@ -243,40 +298,15 @@ class ThreeDEdgeDetector:
 
         self.create_model()
 
-        data = next(iter(self.trainDataset))
-        pred = self.model(data[0])
-        print(pred)
-        # tensorboardCallback = tfk.callbacks.TensorBoard(log_dir=self.logDir, histogram_freq=1)
-        #
-        # self.model.fit(x=data[0],
-        #                y=data[0],
-        #                epochs=self.epochs,
-        #                validation_data=(self.valDataset, self.valDataset),
-        #                callbacks=[tensorboardCallback])
-        # print(tf.shape(data[0]))
-        # layer = MergePointCloud([32, 32, 32], 'sum')
-        # out = layer([data[0], data[0]])
-        # print(tf.shape(out))
-        # print(out)
+        # data = next(iter(self.trainDataset))
+        # out = self.model(data[0])
+        # out = out.numpy()
 
-        # if self.usePreTrained:
-        #     self.load_weights()
-        #
-        # if self.isTrain:
-        #     self.train()
-        # else:
-        #     self.predict()
+        if self.usePreTrained:
+            self.load_weights()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if self.isTrain:
+            self.train()
+            self.test()
+        else:
+            self.predict()
